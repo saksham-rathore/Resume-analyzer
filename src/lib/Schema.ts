e804116315e0a5daab6ca19dfed5bb461288a64a
saksham762007 <samrathore07062007@gmail.com>
2026-05-18 17:23:04 +0530

    feat: initialize better-auth configuration and define user database schema

---
+++ src/lib/Schema.ts
@@ -0,0 +1,8 @@
+import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: text("id").primaryKey(),
+  email: text("email").notNull().unique(),
+  password: text("password"),
+  createdAt: timestamp("created_at").defaultNow(),
+});
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,16 @@
 import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "./db";
 
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
 
+  emailAndPassword: {
+    enabled: true,
+  },
 
-export const auth = betterAuth({});
+  secret: process.env.BETTER_AUTH_SECRET!,
+  baseURL: process.env.BETTER_AUTH_URL!,
+});
