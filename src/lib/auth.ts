fcba1834090dbf16205952af2bf9afba8e1fd467
saksham762007 <samrathore07062007@gmail.com>
2026-05-18 17:56:20 +0530

    feat: implement Better Auth with Drizzle ORM and database schema definition

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,12 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db";
+import * as schema from "./Schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
+    schema,
   }),
 
   emailAndPassword: {
