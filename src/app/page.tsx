7641ae4662a64f237269c4dc3ecfa08ddbb8d4f0
saksham762007 <samrathore07062007@gmail.com>
2026-05-25 01:06:04 +0530

    feat: initialize project with basic home page and custom design system globals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,10 @@
-import Footer from "@/Components/Layout/Footer";
-import Navbar from "@/Components/Layout/Navbar";
-import Banners from "@/Components/Section/Banners";
-import CTA from "@/Components/Section/CTA";
-import Hero from "@/Components/Section/Hero";
-import Features from "@/Components/UI/Features";
 import Image from "next/image";
 import React from "react";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      <Hero />
-      <Banners />
-      <Features />
-      <CTA />
-      <Footer />
+
     </div>
   );
 }
