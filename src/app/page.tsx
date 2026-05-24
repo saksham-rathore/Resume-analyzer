63f77b9251cd591ee197ff60bfeb0a088ea0178e
saksham762007 <samrathore07062007@gmail.com>
2026-05-25 00:51:08 +0530

    feat: implement homepage layout with hero, feature, and connector sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
+import Footer from "@/Components/Layout/Footer";
+import Navbar from "@/Components/Layout/Navbar";
+import Banners from "@/Components/Section/Banners";
+import Hero from "@/Components/Section/Hero";
+import Connectors from "@/Components/UI/Connectors";
+import Features from "@/Components/UI/Features";
 import Image from "next/image";
 import React from "react";
 
 export default function Home() {
   return (
     <div>
-
+      <Navbar />
+      <Hero />
+      <Banners />
+      <Connectors />
+      <Features />
+      <Footer />
     </div>
   );
 }
